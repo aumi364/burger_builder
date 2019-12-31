3dde77cd642bf53b8976c47ea453fc4ab47e8207
aumi364 <aumi.asif@gmail.com>
2020-01-01 00:50:44 +0600

    burgeringredient done

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import AUX from "../../Hoc/Auxilary";
+import Burger from "../../Components/Burger/Burger";
+import BurgerIngredient from "../../Components/Burger/BurgerIngredient/BurgerIngredient";
 export default class BurgerBuilder extends Component {
   render() {
     return (
       <AUX>
-        <div>burger</div>
+        <Burger />
         <div>build Control</div>
       </AUX>
     );
